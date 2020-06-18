0f5c36faad3aef016b072628dba3c58c1caa6608
Patrick Erichsen <patrick.a.erichsen@gmail.com>
2020-06-17 21:51:20 -0500

    Write data to CSV
    Signed-off-by: Patrick Erichsen <patrick.a.erichsen@gmail.com>

--- README.md
+++ README.md
@@ -6,6 +6,4 @@ Program to parse WiFi data exported in JSON format from PrivateKit and calculate
 
 ```sh
 $ python3 parse.py data/sample_data.json
-
-> bssid: 02:15:b2:00:01:00 rssi variance: 12.5
 ```
--- parse.py
+++ parse.py
@@ -4,10 +4,16 @@
 # Program to parse WiFi data exported in JSON format
 # from PrivateKit and calculate the variance in RSSI
 # for access points.
+#
+# Data is written to a CSV file with the same name as the JSON
+# file.
 # -----------------------------------------------------------
 
 import json
 import sys
+import csv
+import os
+from pathlib import Path
 from statistics import variance
 
 
@@ -67,11 +73,50 @@ def calculate_rssi_var(bssid, data):
     return variance(rssi_values)
 
 
+def get_bssid_vars(bssids, data):
+    '''
+    Returns an array of tuples where the first index is the bssid,
+    and the second index is the variance for that bssid
+    '''
+
+    bssid_vars = []
+
+    for bssid in bssids:
+        var = calculate_rssi_var(bssid, data)
+        bssid_vars.append([bssid, var])
+
+    return bssid_vars
+
+
+def write_to_csv(file_name, bssid_var_tuple):
+    '''
+    Write the tuple data from `data` to the csv `file_name`
+    '''
+
+    try:
+        with open(file_name, 'w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(["MAC Address", "Variance"])
+
+            for data in bssid_var_tuple:
+                writer.writerow([data[0], data[1]])
+
+        print('successfully wrote BSSID variance data to', file_name)
+    except:
+        os.remove(file_name)
+
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise ValueError("a path to the json data file must be passed")
 
     file_path = sys.argv[1]
+    file_name = os.path.basename(file_path).split('.')[0] + '.csv'
+
+    # CSV file we will try to write to already exists - error out
+    if Path(file_name).is_file():
+        print('overwriting', file_name)
+        os.remove(file_name)
 
     file = open(file_path)
     data = json.load(file)
@@ -83,6 +128,6 @@ if __name__ == "__main__":
     intersected_wifi_data = remove_non_intersection_scans(
         bssid_intersection, wifi_data)
 
-    for bssid in bssid_intersection:
-        var = calculate_rssi_var(bssid, intersected_wifi_data)
-        print('bssid:', bssid, 'rssi variance:', var)
+    bssid_vars = get_bssid_vars(bssid_intersection, intersected_wifi_data)
+
+    write_to_csv(file_name, bssid_vars)
